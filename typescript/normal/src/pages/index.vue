9fe02d19a5857d09369cbb6323d5edc3a2a9ce88
RequireSun <kelvinlpsun@hotmail.com>
2020-01-14 17:43:08 +0800

    feat: 代码片按行引入功能

--- typescript/normal/src/pages/index.vue
+++ typescript/normal/src/pages/index.vue
@@ -2,10 +2,14 @@
     <div>
         <h1>自定义组件</h1>
         <h2>IDEA & VSCode 都不会报错的方法</h2>
+        <em>idea-component.vue</em>
         <!-- WEBPACK_SNIPPET_LOADER ../components/idea-component.vue -->
         <idea-component ref="ic"></idea-component>
+        <em>index.vue</em>
+        <!-- WEBPACK_SNIPPET_LOADER ./index.vue#L1-L2L7L10L16-L18L19-L21L23-L26L28-L32L34-L39L46-L47 -->
         <button @click="handleClickIdea">click +1 (outer)</button>
         <h2>IDEA 会报错 VSCode 不会报错的方法</h2>
+        <em>vscode-component.vue</em>
         <!-- WEBPACK_SNIPPET_LOADER ../components/vscode-component.vue -->
         <vscode-component ref="vc"></vscode-component>
         <button @click="handleClickVscode">click +1 (outer)</button>
--- typescript/webpack-snippet-loader/index.js
+++ typescript/webpack-snippet-loader/index.js
@@ -1,12 +1,14 @@
 /**
  * 很简单的功能, 没必要用语法解析树, 以后用到再改
+ * 如何编写 loader
+ * https://webpack.docschina.org/contribute/writing-a-loader/
  */
 // const { getOptions } = require('loader-utils');
 const fs = require('fs');
 const path = require('path');
 const hljs = require('highlight.js/lib/highlight.js');
 const hljsDefineVue = require('highlightjs-vue/dist/highlightjs-vue.esm');
-const regex = /\s*<!--\s*WEBPACK_SNIPPET_LOADER\s*([\w-./]+)\s*-->\s*/g;
+const regex = /\s*<!--\s*WEBPACK_SNIPPET_LOADER\s*([\w-./#]+)\s*-->\s*/g;
 
 hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
@@ -39,9 +41,54 @@ module.exports = function(source) {
     const { resourcePath } = this;
 
     return source.replace(regex, (allTag, filePath, startPos, allStr) => {
+        let parts;
+
+        if (filePath.includes('#')) {
+            const strParts = filePath.slice(filePath.indexOf('#') + 1);
+            filePath = filePath.slice(0, filePath.indexOf('#'));
+            parts = [];
+
+            const regexParts = /(L\d+(?:-L\d+)?)/g;
+            let regRes;
+
+            while(regRes = regexParts.exec(strParts)) {
+                parts.push(regRes[0]);
+            }
+
+            parts = parts.map(part => {
+                if (part.includes('-')) {
+                    const [start, end] = part.split('-');
+
+                    return {
+                        start: start.replace(/^L/, ''),
+                        end: end.replace(/^L/, ''),
+                    };
+                } else {
+                    return {
+                        start: part.replace(/^L/, ''),
+                        end: part.replace(/^L/, ''),
+                    };
+                }
+            });
+
+            if (!parts.length) {
+                parts = undefined;
+            }
+        }
+
         filePath = path.resolve(path.dirname(resourcePath), filePath);
-        return `<pre code-snippet><code>${vueQuoteEncode(hljs.highlight('vue', fs.readFileSync(filePath, {
+        let fileContent = fs.readFileSync(filePath, {
             encoding: 'utf-8',
-        })).value)}</code></pre>`;
+        });
+
+        if (parts) {
+            fileContent = fileContent.split('\n');
+            fileContent = parts.map(({ start, end }) => {
+                // 因为行号从 0 开始, 故开始位置 -1
+                return fileContent.slice(start - 1, end).join('\n');
+            }).join('\n');
+        }
+
+        return `<pre code-snippet><code>${vueQuoteEncode(hljs.highlight('vue', fileContent).value)}</code></pre>`;
     });
 };
