0da2162e32e3c441567c6cf43569cd507f0ccc1f
RequireSun <kelvinlpsun@hotmail.com>
2020-01-14 18:24:07 +0800

    feat: 更加牛逼的标记引入法

--- typescript/normal/src/pages/index.vue
+++ typescript/normal/src/pages/index.vue
@@ -1,47 +1,63 @@
+<!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA start:VSCODE -->
 <template>
     <div>
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA end:VSCODE -->
         <h1>自定义组件</h1>
         <h2>IDEA & VSCode 都不会报错的方法</h2>
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA -->
         <em>idea-component.vue</em>
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA -->
         <!-- WEBPACK_SNIPPET_LOADER ../components/idea-component.vue -->
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA -->
         <idea-component ref="ic"></idea-component>
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA -->
         <em>index.vue</em>
-        <!-- WEBPACK_SNIPPET_LOADER ./index.vue#L1-L2L7L10L16-L18L19-L21L23-L26L28-L32L34-L39L46-L47 -->
+        <!-- WEBPACK_SNIPPET_LOADER ./index.vue#TAG:IDEA -->
         <button @click="handleClickIdea">click +1 (outer)</button>
         <h2>IDEA 会报错 VSCode 不会报错的方法</h2>
         <em>vscode-component.vue</em>
         <!-- WEBPACK_SNIPPET_LOADER ../components/vscode-component.vue -->
         <vscode-component ref="vc"></vscode-component>
         <button @click="handleClickVscode">click +1 (outer)</button>
+        <!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA -->
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import IdeaComponent, { FixComponent as FixIdeaComponent } from '../components/idea-component.vue';
+/* WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA */
 import VscodeComponent from '../components/vscode-component.vue';
+// WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA
 
 @Component({
     components: {
         IdeaComponent,
+        // WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA
         VscodeComponent,
+        /* WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA */
     },
 })
 export default class Index extends Vue {
     $refs!: {
         ic: FixIdeaComponent,
+        /* WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA */
         vc: VscodeComponent,
+        /* WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA */
     };
 
     handleClickIdea() {
         // VSCode IDEA 都正常工作
         this.$refs.ic.increase();
     }
+    /* WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA */
 
     handleClickVscode() {
         // VSCode 正常工作
         // IDEA 会提示找不到方法的错误 (TS2339), 但是 ctrl + 左键还是能正常跳转到定义位置
         this.$refs.vc.increase();
     }
+    /* WEBPACK_SNIPPET_LOADER_GUIDEPOST start:IDEA */
 }
 </script>
+<!-- WEBPACK_SNIPPET_LOADER_GUIDEPOST end:IDEA -->
--- typescript/webpack-snippet-loader/index.js
+++ typescript/webpack-snippet-loader/index.js
@@ -8,7 +8,11 @@ const fs = require('fs');
 const path = require('path');
 const hljs = require('highlight.js/lib/highlight.js');
 const hljsDefineVue = require('highlightjs-vue/dist/highlightjs-vue.esm');
-const regex = /\s*<!--\s*WEBPACK_SNIPPET_LOADER\s*([\w-./#]+)\s*-->\s*/g;
+
+const regex = /^\s*<!--\s*WEBPACK_SNIPPET_LOADER\s*([\w-./#:]+)\s*-->\s*$/gm;
+const regexGuidepostHtml = /^\s*<!--\s*WEBPACK_SNIPPET_LOADER_GUIDEPOST((?:\s(?:start|end):\w+)+)\s*-->\s*$/m;
+const regexGuidepostJavascript1 = /^\s*\/\*\s*WEBPACK_SNIPPET_LOADER_GUIDEPOST((?:\s(?:start|end):\w+)+)\s*\*\/\s*$/m;
+const regexGuidepostJavascript2 = /^\s*\/\/\s*WEBPACK_SNIPPET_LOADER_GUIDEPOST((?:\s(?:start|end):\w+)+)\s*$/m;
 
 hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
@@ -42,37 +46,43 @@ module.exports = function(source) {
 
     return source.replace(regex, (allTag, filePath, startPos, allStr) => {
         let parts;
+        let tag;
 
         if (filePath.includes('#')) {
             const strParts = filePath.slice(filePath.indexOf('#') + 1);
             filePath = filePath.slice(0, filePath.indexOf('#'));
-            parts = [];
 
-            const regexParts = /(L\d+(?:-L\d+)?)/g;
-            let regRes;
+            if (strParts.startsWith('TAG:')) {
+                tag = strParts.replace(/^TAG:/, '');
+            } else {
+                parts = [];
 
-            while(regRes = regexParts.exec(strParts)) {
-                parts.push(regRes[0]);
-            }
+                const regexParts = /(L\d+(?:-L\d+)?)/g;
+                let regRes;
 
-            parts = parts.map(part => {
-                if (part.includes('-')) {
-                    const [start, end] = part.split('-');
-
-                    return {
-                        start: start.replace(/^L/, ''),
-                        end: end.replace(/^L/, ''),
-                    };
-                } else {
-                    return {
-                        start: part.replace(/^L/, ''),
-                        end: part.replace(/^L/, ''),
-                    };
+                while(regRes = regexParts.exec(strParts)) {
+                    parts.push(regRes[0]);
                 }
-            });
 
-            if (!parts.length) {
-                parts = undefined;
+                parts = parts.map(part => {
+                    if (part.includes('-')) {
+                        const [start, end] = part.split('-');
+
+                        return {
+                            start: start.replace(/^L/, ''),
+                            end: end.replace(/^L/, ''),
+                        };
+                    } else {
+                        return {
+                            start: part.replace(/^L/, ''),
+                            end: part.replace(/^L/, ''),
+                        };
+                    }
+                });
+
+                if (!parts.length) {
+                    parts = undefined;
+                }
             }
         }
 
@@ -87,6 +97,40 @@ module.exports = function(source) {
                 // 因为行号从 0 开始, 故开始位置 -1
                 return fileContent.slice(start - 1, end).join('\n');
             }).join('\n');
+        }
+
+        if (tag) {
+            const regexTag = new RegExp(`^(start|end):${tag}$`);
+            fileContent = fileContent.split('\n');
+            let isStart = false;
+
+            fileContent = fileContent.reduce((lines, cur, index) => {
+                let matchRes;
+
+                // 利用 if 语句的阻断原理, 某一步匹配到了, 后面的 match 就不会执行了
+                if (matchRes = cur.match(regexGuidepostHtml)) {
+                } else if (matchRes = cur.match(regexGuidepostJavascript1)) {
+                } else if (matchRes = cur.match(regexGuidepostJavascript2)) {
+                }
+
+                if (matchRes) {
+                    // 分拆寻找 tag
+                    const tags = matchRes[1].split(' ').filter(item => item);
+                    const stat = tags.find(_t => regexTag.test(_t));
+                    if (stat) {
+                        // true 开始 false 结束 其他不管
+                        isStart = stat.startsWith('start') ? true : (stat.startsWith('end') ? false : isStart);
+                    }
+                    // 如果本行是标记行, 跳过, 不要加到输出里去
+                    return lines;
+                }
+
+                if (isStart) {
+                    lines.push(cur);
+                }
+
+                return lines;
+            }, []).join('\n');
         }
 
         return `<pre code-snippet><code>${vueQuoteEncode(hljs.highlight('vue', fileContent).value)}</code></pre>`;
