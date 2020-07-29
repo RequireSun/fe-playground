23113c201fe04209f85f08eb2f33515af164aeb4
RequireSun <kelvinlpsun@hotmail.com>
2020-07-29 20:27:51 +0800

    feat: 完善一下声明到全局的 case

--- scope-chain/index.js
+++ scope-chain/index.js
@@ -1,3 +1,5 @@
+// 直接写到 global 上也可以
+global.b = 2;
 // 隐式声明到 global 上就可以读到
 c = 3;
 // var 到作用域上就不行
@@ -6,18 +8,26 @@ var d = 4;
 // 这里和直接声明一个函数时绑定的作用域链相同
 const foo = new Function(
     'a',
-    'b',
-    `try { console.log(a, b, c, d); } catch(ex) { console.error(ex); } return a + b + c;`
+    `
+console.log(a);
+console.log(b);
+console.log(c);
+try { 
+    console.log(d); 
+} catch(ex) { 
+    console.error('d error'); 
+} `
 );
-console.log(foo(1, 2));
+foo(1);
 
 const bar = function (fn) {
+    var b = 3;
     var c = 4;
     var d = 5;
-    return fn(1, 2);
+    return fn(1);
 };
 
-console.log(bar(foo));
+bar(foo);
 
 var e = 1;
 var f = 2;
